b4d63e6d186362e99f42cd07d59708dd66484cf1
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2019-07-06 23:11:59 -0300

    base de geometria adicionada

---
+++ Geometry/base.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef double T; // mudar para o tipo a ser usado
+struct pv{
+    T x,y;
+
+    // operacoes binarias
+    pv operator+(pv p){ return {x+p.x, y+p.y};} // soma
+    pv operator-(pv p){ return {x-p.x, y-p.y};} // subtracao
+    pv operator*(T k){ return {p.x*k, p.y*k};} // multiplicacao por constante
+    T dot(pv v){ return x*v.x + y*v.y;} // produto escalar
+    pv cross(pv v){return {x*v.y - y*v.x};} // produto vetorial
+
+    // relacoes binarias
+    bool operator==(pv p){ return (x == p.x) && (y == p.y);} // igualdade / checar se precisa do eps
+    bool operator!=(pv p){ return !(*this==p);} // dif
+
+    // angulo
+    double angle_pe(pv v){ // angulo usando a definicao de produto escalar
+        double cosa = max(min(dot(v) / norml2() / v.norml2(), 1.0), -1.0);
+        return acos(cosa);
+    }
+    double angle_pv(pv v){ // angulo usando a definicao de produto escalar
+        double sena = max(min(cross(v) / norml2() / v.norml2(), 1.0), -1.0);
+        return asin(cosa);
+    }
+
+    // orientacao
+    // se ret < 0: sentido horario
+    // se ret < 0: sentido anti horario
+    // se ret == 0; colineares
+    T orient(pv a, pv b){ return (a-(*this)).cross(b-(*this)); }
+
+    // normas (operacoes unarias)
+    T sq(void){return x*x+y*y;} //
+    T norml0(){ return (x!=0)+(y!=0);} // quantos elementos nao nulos
+    T norml1(){ return abs(x)+abs(y);} // distancia de manhattan
+    double norml2(){ return sqrt(sq());} // norma euclidiana
+    T normlinf(){ return max(abs(x),abs(y));} // norma infinita
+
+    // transformacoes
+    // rotaciona o vetor em a radianos
+    pt rotate(double a){ return {x*cos(a) - y*sin(a), x*sin(a) + y*cos(a)}; }
+
+    // dentro de ...
+    bool inAngle(pv a, pv b, pv c){
+        // checa se o ponto ta dentro do angulo def por a,b,c
+        if(a.orient(b,c) == 0) printf("COLINEARES\n"); // da ruim se forem colineares
+        if(a.orient(b,c) < 0) swap(b,c); // resumir pra um caso
+        return (a.orient(b,p) >= 0) && (a.orient(c,p) <= 0); // se ta dentro
+    }
+}
+
+// funcao pra checar se poligono eh convexo
+// assume que p eh dado com os pontos ordenados
+bool isConvex(vector<pv> p){
+    bool hasPos = false, hasNeg = false;
+    for(int i = 0; i < p.size(); ++i){
+        int temp = p[i].orient(p[(i+1)%p.size()], p[(i+2)%p.size()]); // checnado atual e dois prox
+        if(temp < 0) hasNeg = true; // se os pontos sao dados em sentido horario
+        if(temp > 0) hasPos = true; // se os pontos sao dados em sentido antihorario
+    }
+    return !(hasNeg && hasPos); // se tem horario e antihorario ta ruim
+}
+
+
+// POLAR SORT
