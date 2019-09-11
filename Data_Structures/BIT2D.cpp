7d1f800f901256f77233e35e399491fc4b0b53dc
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2019-09-11 18:31:28 -0300

    bit2d com max(min)

--- Data_Structures/BIT2D.cpp
+++ Data_Structures/BIT2D.cpp
@@ -4,12 +4,12 @@
 void upd(int px, int py, int v){
     for(int x = px+1; x < MAXN; x += x&-x)
     for(int y = py+1; y < MAXN; y += y&-y)
-        bit[x][y]+=v;
+        bit[x][y]+=v; // bit[x][y] = max(bit[x][y], v);
 }
 ll query(int px, int py){
-    ll ret = 0;
+    ll ret = 0; // int ret = MIN;
     for(int x = px+1; x > 0; x -= x&-x)
     for(int y = py+1; y > 0; y -= y&-y)
-        ret += bit[x][y];
+        ret += bit[x][y]; // ret = max(ret, bit[x][y]);
     return ret;
 }
