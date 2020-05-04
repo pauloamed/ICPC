87d6f4c100ecb9376a3b7b8bbb323ca77232d140
andrewinston <a.winston.s97@gmail.com>
2020-05-04 15:09:01 -0300

    now it compiles, and removing size_t's

--- Data_Structures/ufind.cpp
+++ Data_Structures/ufind.cpp
@@ -8,26 +8,26 @@
     - se usar so um dos dois fica O(log n)
 */
 
-size_t sizes[MAXN]; // vetor com tamanhos
-size_t parents[MAXN]; // vetor com pai de cada no
+int sizes[MAXN]; // vetor com tamanhos
+int parents[MAXN]; // vetor com pai de cada no
 // stack<pair<int,int>> old_pai, old_tam; ROLLBACK
 
-void init(size_t n){
-    for(size_t i = 0; i < n; ++i){
+void init(int n){
+    for(int i = 0; i < n; ++i){
         sizes[i] = 1; // tamanho inicial
         parents[i] = i; // valor padrao: cada um eh seu pai
     }
 }
 
-size_t do_find(current){
+int do_find(int current){
     // ROLLBACK DOESNT HAVE PATH COMPRESSION
-    size_t new_root = current; // variavel para guardar nova raiz
+    int new_root = current; // variavel para guardar nova raiz
     while(new_root != parents[new_root]) // enquanto nao encontro no raiz
         new_root = parents[new_root]; // subo na arvore
 
 
     // compressao de caminho (*)
-    size_t next; // backup do pai
+    int next; // backup do pai
     while(parents[current] != new_root){ // enquanto nao acho a nova raiz
         next = parents[current]; // faco o backup do pai
         parents[current] = new_root; // digo que o pai eh a raiz achada (*)
@@ -37,7 +37,7 @@ size_t do_find(current){
     return new_root; // retornamo a raiz da arvore
 }
 
-void do_union(size_t a, size_t b){
+void do_union(int a, int b){
     a = do_find(a); b = do_find(b); // unimos uma raiz a outra
 
     if(a == b) return; // se for a mesma raiz, nao tenho o que unir
@@ -51,6 +51,7 @@ void do_union(size_t a, size_t b){
     sizes[a] += sizes[b]; // atualizando tamanho de "a"
     parents[b] = a; // pai de "b" eh "a"
 }
+
 
 void rollback(){
     sizes[old_tam.top().first] = old_tam.top().second; old_tam.pop();
