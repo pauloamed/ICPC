006c348ba58e5527d1c4ad087cd3707ff6166f91
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2020-07-19 14:47:45 -0300

    Update dijkstra.cpp

--- Graphs/dijkstra.cpp
+++ Graphs/dijkstra.cpp
@@ -4,29 +4,26 @@ using namespace std;
 #define INF 10000000
 #define MAXN 100
 
-size_t dist[MAXN]; // Vetor de distancias
-size_t parent[MAXN]; // Vetor com o pai
-vector<pair<size_t, size_t>> v[100]; // Lista de adjacencia com pesos
-set<pair<size_t,size_t>> s; // Set do Dijkstra com dists e atuais
+int dist[MAXN]; // Vetor de distancias
+int parent[MAXN]; // Vetor com o pai
+vector<pair<int, int>> v[MAXN]; // Lista de adjacencia com pesos
+set<pair<int,int>> s; // Set do Dijkstra com dists e atuais
 
 /*
 Analisa um vertice por vez, aquele com menor distancia ja calculada
 Como nao tem aresta negativa, depois que um vertice for removido, ele nao sera
 mais visitado
-
 COMPLEXIDADE TEMPO:
     HEAP TEM SIMULTANEAMENTE O(n) CARAS
     n OPERACOES DE INSERT E DELETE (log n)
     O(m) OPERACOES DE ATUALIZACAO (log n)
     O((n+n+m)log n)
-
 COMPLEXIDADE ESPACO:
     O(n)
-
 */
 
-void dijkstra(size_t u, size_t n){
-    for(size_t i = 0; i < n; ++i){
+void dijkstra(int u, int n){
+    for(int i = 0; i < n; ++i){
         dist[i] = INF; // inicializando distancias
         parent[i] = i; // inicializando pai
     }
@@ -35,14 +32,14 @@ void dijkstra(size_t u, size_t n){
     set.insert({0, u}); // Adiciono a dist p ele e ele, na heap ordenada por dists
 
     while(!pq.empty()){
-        size_t cur_dist = s.begin()->first; // Recuperando dist pro vertice atual
-        size_t cur_vertice = s.begin()->second; // Recuperando o vertice atual
+        int cur_dist = s.begin()->first; // Recuperando dist pro vertice atual
+        int cur_vertice = s.begin()->second; // Recuperando o vertice atual
         s.erase(s.begin()); // Removendo vertice atual do set, cada vertice passa por isso 1
         // vez, ja que nao ha aresta negativa
 
-        for(size_t i = 0; i < v[cur_vertice].size(); i++){ // Pra cada adjacente
+        for(int i = 0; i < v[cur_vertice].size(); i++){ // Pra cada adjacente
             // Distancia pro adjacente
-            size_t dist_adj = cur_dist + v[cur_vertice][i].second;
+            int dist_adj = cur_dist + v[cur_vertice][i].second;
 
             // Se a dist computada pro adj for menor que a armazenada
             if(dist_adj < dist[v[cur_vertice][i].first]){
