1cbb31e86566da4693ee4d548db4f01bcb5a1f14
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2020-08-12 11:19:20 -0300

    Update lca.cpp

--- Graphs/lca.cpp
+++ Graphs/lca.cpp
@@ -1,23 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define MAXV 2100000
-#define MAXLOG 23
-#define LINHA 1010
+#define MAXV 100100
+#define MAXLOG 17
+#define INF 1e9
 
 vector<int> v[MAXV]; // lista de adj
 
 int st[MAXV][MAXLOG]; // sparse table (x,i) guardando ancestral de ordem 2^i de x
 int lvl[MAXV]; // lvl=profundidade de cada no
+bool vis_lca[MAXV]; // vis
 
-void init_dfs(int x, int pai, bool jafoi[]){
-    jafoi[x] = true; // marcando vertice como visitado
-    st[x][0] = pai; // o ancestral de ordem 2^0 (1) de x eh o pai dele
+void init_dfs(int x, int par){
+    vis_lca[x] = true; // marcando vertice como visitado
+    st[x][0] = par; // o ancestral de ordem 2^0 (1) de x eh o pai dele
     // se tiver pai valido (!= raiz), a prof aumenta em 1
-    if(pai != -1) lvl[x] = lvl[pai]+1;
+    if(par != -1) lvl[x] = lvl[par] + 1;
 
     for(int i = 0; i < v[x].size(); ++i) // visitar todos adjacentes
-        if(!jafoi[v[x][i]]) init_dfs(v[x][i], x); // passo recursivo
+        if(!vis_lca[v[x][i]]) init_dfs(v[x][i], x); // passo recursivo
 }
 
 void init_st(){
@@ -30,15 +31,14 @@ void init_st(){
 }
 
 void init(int root, int n){ // funcao init, raiz e #nos
-    bool *jafoi = new bool[n]; // visitados
     // botando que o pai da raiz eh -1, pode ser ela mesma tbm
-    init_dfs(root, -1, jafoi); // calcular os ancestrais imediatos
-    delete[] jafoi;
+    init_dfs(root, -1); // calcular os ancestrais imediatos
     init_st(); // init da sparse table
 }
 
 
 int lca(int x, int y){ // lca de x e y
+    // cout << lvl[x] << " " << lvl[y] << endl;
     if(lvl[x] < lvl[y]) swap(x, y); // quero q x seja mais profundo q y
 
     int falta_subir = (lvl[x] - lvl[y]); // igualo as profundidades
@@ -63,4 +63,9 @@ int lca(int x, int y){ // lca de x e y
     }
 
     return st[x][0]; // retornando o ponto de encontro
+}
+
+int _dist(int a, int b){
+    int x = lca(a, b);
+    return (lvl[a] - lvl[x]) + (lvl[b] - lvl[x]);
 }
