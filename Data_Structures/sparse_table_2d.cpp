bfdfb01aeea8e53c98e3c97ae62d38c73b5be83e
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2020-09-03 11:42:56 -0300

    Update sparse_table_2d.cpp

--- Data_Structures/sparse_table_2d.cpp
+++ Data_Structures/sparse_table_2d.cpp
@@ -1,11 +1,10 @@
-// https://www.codechef.com/submit/CHSQARR
-
 #include<bits/stdc++.h>
 using namespace std;
 
 #define MAXN 1010
 #define K 12
 #define INF 1e9
+
 
 int sum[MAXN][MAXN];
 int vlog[MAXN];
@@ -22,8 +21,8 @@ void precalc(int n, int m){
         for(int k = 1; k < K; ++k){ // para cada tam de intervalo
             int j = 0;
             int interval_size = (1 << k); // tamanho do intervalo def pelo for externo
+            int last_invert_size = (1 << (k-1)); // recuperando tam do interv anterior
             while(j + interval_size <= m){ // enquanto o interv. estiver num pos valida
-                int last_invert_size = (1 << (k-1)); // recuperando tam do interv anterior
 
                 // calculo o intervalo atual somando as metades
                 // uma comecando em j e outra em j+((2^j)/2)
@@ -39,8 +38,8 @@ void precalc(int n, int m){
             for(int k = 1; k < K; ++k){ // para cada tam de intervalo
                 int i = 0;
                 int interval_size = (1 << k); // tamanho do intervalo def pelo for externo
+                int last_invert_size = (1 << (k-1)); // recuperando tam do interv anterior
                 while(i + interval_size <= n){ // enquanto o interv. estiver num pos valida
-                    int last_invert_size = (1 << (k-1)); // recuperando tam do interv anterior
 
                     // calculo o intervalo atual somando as metades
                     // uma comecando em j e outra em j+((2^j)/2)
@@ -54,40 +53,29 @@ void precalc(int n, int m){
 
 
 int query(int u, int l, int d, int r){
-    // O(1)
-    int interv_size_hor = r - l + 1; // recuperando o tamanho do intervalo
-    int interv_size_ver = d - u + 1; // recuperando o tamanho do intervalo
-    int hor = vlog[interv_size_hor]; // recuperando pot que def o tam do interv.
-    int ver = vlog[interv_size_ver]; // recuperando pot que def o tam do interv.
-    // tamanho de intervalo que vai ser considerado ja que o log eh capado
-    int used_interv_size_hor = (1 << hor);
-    int used_interv_size_ver = (1 << ver);
-
     /*
     A----B----
     ----------
     C----D----
     */
-
-    vector<pair<int,int>> points = {
-        {u, l},
-        {u, r - used_interv_size_hor + 1},
-        {d - used_interv_size_ver + 1, l},
-        {d - used_interv_size_ver + 1, r - used_interv_size_hor + 1}
-    };
-
-    int ans = -INF;
-    for(auto x : points){
-        ans = max(ans, st[x.first][x.second][ver][hor]);
-    }
+    // O(1)
+    int hor = vlog[r - l + 1]; // recuperando pot que def o tam do interv.
+    int ver = vlog[d - u + 1]; // recuperando pot que def o tam do interv.
+    // tamanho de intervalo que vai ser considerado ja que o log eh capado
+    d = d - (1 << ver) + 1;
+    r = r - (1 << hor) + 1;
+    int ans = st[u][l][ver][hor];
+    ans = max(ans, st[u][r][ver][hor]);
+    ans = max(ans, st[d][l][ver][hor]);
+    ans = max(ans, st[d][r][ver][hor]);
     return ans;
 }
 
 int sumQuery(int u, int l, int d, int r, int n, int m){
     int a = sum[u][l];
-    int b = 0; if(r + 1 < m) b = sum[u][r + 1];
-    int c = 0; if(d + 1 < n) c = sum[d + 1][l];
-    int e = 0; if(r + 1 < m && d + 1 < n) e = sum[d + 1][r + 1];
+    int b = sum[u][r + 1];
+    int c = sum[d + 1][l];
+    int e = sum[d + 1][r + 1];
     return a - (b + c) + e;
 }
 
@@ -104,7 +92,7 @@ int32_t main(){
 
     for(int i = n - 1; i >= 0; --i){
         for(int j = m - 1; j >= 0; --j){
-            int val = matrix[i][j];
+            int val = st[i][j][0][0];
             int a = 0, b = 0, c = 0;
             if(i < n - 1) a = sum[i + 1][j];
             if(j < m - 1) b = sum[i][j + 1];
