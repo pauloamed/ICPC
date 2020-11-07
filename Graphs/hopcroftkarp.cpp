21d4d904c61051bfeb006fbf6222a8e560796764
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2020-11-07 11:55:35 -0300

    Update hopcroftkarp.cpp

--- Graphs/hopcroftkarp.cpp
+++ Graphs/hopcroftkarp.cpp
@@ -1,89 +1,99 @@
 // implements hopcroft karp algorithm and vertex cover for bipartite graph
 // hopcroft karp runs in O(Vsqrt(E))
-// to use it, just run prepare() with the size of the two partitions of the graph
-// then add the edges with the function addEdge()
+// to use it, just run the constructor with the size of the two partitions of the graph
+// then add the edges with the function addEdge(), from partition U to V
 // note that the vertices must be 1-indexed, as the 0 value is used for a dummy vertex
+	// also, quite obvious, all indexes i must be inside [1;m] (for U) or [1;n] (for V)
 // code inspired by https://www.geeksforgeeks.org/hopcroft-karp-algorithm-for-maximum-matching-set-2-implementation/
-struct{
-	int n;
-	int m;
-	int NIL=0;
-	const int INF = 2e9;
+const int INF = 2e9;
+const int NIL=0;
+
+// since this is a bipartite graph, there are two vertex sets: U and V. m = size(U), n = size(V).
+// the edges to be added must go from U to V (assume that they are undirected).
+
+// independent set: n - vertex cover
+// each connected component must (?) be executed separatedly
+
+struct bpGraph{
+	int n, m;
 	int *pairU;
 	int *pairV;
 	int *dist;
-	
-	void prepare(int _m, int _n){
-		this->n = _n;
-		this->m = _m;
-		adj = vector<vector<int>>(m+1);
-		radj = vector<vector<int>>(n+1);
-	}
-	//adjacencies from U to V
-	vector<vector<int>> adj;
-	//adjacencies from V to U (used only on vertex cover)
-	vector<vector<int>> radj;
-	
+	  //adjacencies from U to V
+	  vector<vector<int>> adj;
+	  //adjacencies from V to U (used only on vertex cover)
+	  vector<vector<int>> radj;
+
+	  bpGraph(int _m, int _n){
+	    this->n = _n;
+	    this->m = _m;
+	    adj = vector<vector<int>>(m+1);
+	    radj = vector<vector<int>>(n+1);
+	  }
+
 	void addEdge(int u, int v){
 		adj[u].push_back(v);
 		radj[v].push_back(u);
 	}
-	bool dfs(int u){ 
+
+	bool dfs(int u){
 		if (u != NIL){
 		    for(auto v : adj[u]){
-		        if (dist[pairV[v]] == dist[u]+1){ 
-		            if (dfs(pairV[v]) == true){ 
-		                pairV[v] = u; 
-		                pairU[u] = v; 
-		                return true; 
-		            } 
-		        } 
-		    } 
-		    dist[u] = INF; 
-		    return false; 
-		} 
-		return true; 
-	} 
-	bool bfs(){ 
+			if (dist[pairV[v]] == dist[u]+1){
+			    if (dfs(pairV[v]) == true){
+				pairV[v] = u;
+				pairU[u] = v;
+				return true;
+			    }
+			}
+		    }
+		    dist[u] = INF;
+		    return false;
+		}
+		return true;
+	}
+
+	bool bfs(){
 		queue<int> Q;
-		for (int u=1; u<=m; u++){ 
-		    if (pairU[u]==NIL){ 
-		        dist[u] = 0; 
-		        Q.push(u); 
-		    } 
-		    else dist[u] = INF; 
-		} 
-		dist[NIL] = INF; 
-		while (!Q.empty()){ 
-		    int u = Q.front(); 
-		    Q.pop(); 
+		for (int u=1; u<=m; u++){
+		    if (pairU[u]==NIL){
+			dist[u] = 0;
+			Q.push(u);
+		    }
+		    else dist[u] = INF;
+		}
+		dist[NIL] = INF;
+		while (!Q.empty()){
+		    int u = Q.front();
+		    Q.pop();
 		    if (dist[u] < dist[NIL]){
-		    	for(auto v : adj[u]){ 
-		            if (dist[pairV[v]] == INF){ 
-		                dist[pairV[v]] = dist[u] + 1; 
-		                Q.push(pairV[v]); 
-		            } 
-		        } 
-		    } 
-		} 
-		return (dist[NIL] != INF); 
-	} 
-	int hopcroftKarp(){ 
+			for(auto v : adj[u]){
+			    if (dist[pairV[v]] == INF){
+				dist[pairV[v]] = dist[u] + 1;
+				Q.push(pairV[v]);
+			    }
+			}
+		    }
+		}
+		return (dist[NIL] != INF);
+	}
+
+	int hopcroftKarp(){
 		pairU = new int[m+1];
-		pairV = new int[n+1]; 
+		pairV = new int[n+1];
 		dist = new int[m+1];
 
-		for (int u=0; u<=m; u++) pairU[u] = NIL; 
+		for (int u=0; u<=m; u++) pairU[u] = NIL;
 		for (int v=0; v<=n; v++) pairV[v] = NIL;
 
-		int result = 0; 
-		while (bfs()){ 
-			for (int u=1; u<=m; u++) 
-				if (pairU[u]==NIL && dfs(u)) 
-				    result++; 
+		int result = 0;
+		while (bfs()){
+			for (int u=1; u<=m; u++)
+				if (pairU[u]==NIL && dfs(u))
+				    result++;
 		}
 
-		return result; 
+		return result;
 	}
 
 	void alternate(int vertex, set<int> &su, set<int> &sv, int inU){
@@ -119,4 +129,4 @@ struct{
 		}
 		return m-(int)su.size()+(int)sv.size();
 	}
-} bpGraph;
+};
