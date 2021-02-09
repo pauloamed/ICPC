64f4766440f539a47b864b7b96da3b8449267736
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2021-02-09 14:52:18 -0300

    Update sieve_gm.cpp

--- Math/sieve_gm.cpp
+++ Math/sieve_gm.cpp
@@ -48,3 +48,32 @@ void sieve(int n){
         }
     }
 }
+
+
+
+// https://codeforces.com/blog/entry/54090
+std::vector <int> prime;
+bool is_composite[MAXN];
+int func[MAXN], cnt[MAXN];
+
+void sieve (int n) {
+	std::fill (is_composite, is_composite + n, false);
+	func[1] = 1;
+	for (int i = 2; i < n; ++i) {
+		if (!is_composite[i]) {
+			prime.push_back (i);
+			func[i] = 1; cnt[i] = 1;
+		}
+		for (int j = 0; j < prime.size () && i * prime[j] < n; ++j) {
+			is_composite[i * prime[j]] = true;
+			if (i % prime[j] == 0) {
+				func[i * prime[j]] = func[i] / cnt[i] * (cnt[i] + 1);
+				cnt[i * prime[j]] = cnt[i] + 1;
+				break;
+			} else {
+				func[i * prime[j]] = func[i] * func[prime[j]];
+				cnt[i * prime[j]] = 1;
+			}
+		}
+	}
+}
