a226dd35bfdb2c88cb9980d51cd1ac0f90e8bd29
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2021-04-29 17:09:30 -0300

    Update ufind.cpp

--- Data_Structures/ufind.cpp
+++ Data_Structures/ufind.cpp
@@ -1,60 +1,54 @@
-#include<iostream>
+struct DSU{
+  int numComponents;
+  int sizes[MAXN]; // vetor com tamanhos
+  int parents[MAXN]; // vetor com pai de cada no
+  stack<pair<int,int>> lastPar, lastSize;
+  vector<int> checkpoints;
 
-#define MAXN 100
+  DSU(int n){
+    checkpoints.push_back(0);
+    numComponents = n;
+      for(int i = 0; i < n; ++i){
+          sizes[i] = 1; // tamanho inicial
+          parents[i] = i; // valor padrao: cada um eh seu pai
+      }
+  }
 
-/*
- - union find (disjoint union set) com compressao de caminho e uniao por tamanhos
-    - acaba ficando O(1) amortizada
-    - se usar so um dos dois fica O(log n)
-*/
+  int find(int current){
+      int newRoot = current; // variavel para guardar nova raiz
+      while(newRoot != parents[newRoot]) // enquanto nao encontro no raiz
+          newRoot = parents[newRoot]; // subo na arvore
+      return newRoot; // retornamo a raiz da arvore
+  }
 
-int sizes[MAXN]; // vetor com tamanhos
-int parents[MAXN]; // vetor com pai de cada no
-// stack<pair<int,int>> old_pai, old_tam; ROLLBACK
+  void onion(int a, int b){
+      a = find(a); b = find(b); // unimos uma raiz a outra
 
-void init(int n){
-    for(int i = 0; i < n; ++i){
-        sizes[i] = 1; // tamanho inicial
-        parents[i] = i; // valor padrao: cada um eh seu pai
+      if(a == b) return; // se for a mesma raiz, nao tenho o que unir
+
+      // uniao por tamanho
+      if(sizes[a] < sizes[b]) swap(a,b); // quero unir "b" a "a"
+
+      checkpoints.back()++;
+      lastSize.push({a, sizes[a]});
+      lastPar.push({b, parents[b]});
+
+      sizes[a] += sizes[b]; // atualizando tamanho de "a"
+      parents[b] = a; // pai de "b" eh "a"
+      numComponents--;
+  }
+
+  void check(){
+    checkpoints.push_back(0);
+  }
+
+  void rollback(){
+    int x = checkpoints.back();
+    numComponents += x;
+    while (x--) {
+      sizes[lastSize.top().first] = lastSize.top().second; lastSize.pop();
+      parents[lastPar.top().first] = lastPar.top().second; lastPar.pop();
     }
-}
-
-int do_find(int current){
-    // ROLLBACK DOESNT HAVE PATH COMPRESSION
-    int new_root = current; // variavel para guardar nova raiz
-    while(new_root != parents[new_root]) // enquanto nao encontro no raiz
-        new_root = parents[new_root]; // subo na arvore
-
-
-    // compressao de caminho (*)
-    int next; // backup do pai
-    while(parents[current] != new_root){ // enquanto nao acho a nova raiz
-        next = parents[current]; // faco o backup do pai
-        parents[current] = new_root; // digo que o pai eh a raiz achada (*)
-        current = next; // subo na arvore pro backup
-    }
-
-    return new_root; // retornamo a raiz da arvore
-}
-
-void do_union(int a, int b){
-    a = do_find(a); b = do_find(b); // unimos uma raiz a outra
-
-    if(a == b) return; // se for a mesma raiz, nao tenho o que unir
-
-    // uniao por tamanho
-    if(sizes[a] < sizes[b]) swap(a,b); // quero unir "b" a "a"
- 
-    // old_tam.push({a, sizes[a]}); ROLLBACK
-    // old_pai.push({b, parents[b]}); ROLLBACK
- 
-    sizes[a] += sizes[b]; // atualizando tamanho de "a"
-    parents[b] = a; // pai de "b" eh "a"
-}
-
-
-void rollback(){
-    sizes[old_tam.top().first] = old_tam.top().second; old_tam.pop();
-    parents[old_pai.top().first] = old_pai.top().second; old_pai.pop();
-}
-
+    checkpoints.pop_back();
+  }
+};
