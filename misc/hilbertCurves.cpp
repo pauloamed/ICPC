23fd26a361215d8f01dea980a01f39a393c4168a
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2021-06-07 12:39:14 -0300

    Update hilbertCurves.cpp

--- misc/hilbertCurves.cpp
+++ misc/hilbertCurves.cpp
@@ -1,15 +1,16 @@
-// https://codeforces.com/blog/entry/61203
+// src: https://codeforces.com/blog/entry/61203
+// code passes: https://codeforces.com/contest/86/problem/D
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LOG_MAXN 19
+#define MAXN 1000010
 
 inline int64_t gilbertOrder(int x, int y, int pow, int rotate) {
-	if (pow == 0) {
-		return 0;
-	}
+	if (pow == 0) return 0;
 	int hpow = 1 << (pow-1);
-	int seg = (x < hpow) ? (
-		(y < hpow) ? 0 : 3
-	) : (
-		(y < hpow) ? 1 : 2
-	);
+	int seg = (x < hpow) ? ((y < hpow) ? 0 : 3) : ((y < hpow) ? 1 : 2);
 	seg = (seg + rotate) & 3;
 	const int rotateDelta[4] = {3, 0, 0, 1};
 	int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
@@ -20,12 +21,72 @@ inline int64_t gilbertOrder(int x, int y, int pow, int rotate) {
 	ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
 	return ans;
 }
- 
+
 struct Query {
-	int l, r, idx;
+	int l, r, idx; // 1 indexado
 	int64_t ord;
- 
+
 	inline void calcOrder() {
-		ord = gilbertOrder(l, r, 21, 0);
+		ord = gilbertOrder(l, r, LOG_MAXN, 0);
 	}
 };
+
+#define ll long long
+ll curr;
+vector<int> v;
+int cnt[MAXN];
+
+int walkRLeft(int r){
+  // removing v[r]
+  ll x = 2 * (cnt[v[r]]--) - 1;
+  curr -= x * v[r];
+  return r - 1;
+}
+
+int walkRRight(int r){
+  // adding v[r + 1]
+  ll x = 2 * (cnt[v[++r]]++) + 1;
+  curr += x * v[r];
+  return r;
+}
+
+int walkLLeft(int l){
+  // adding v[l - 1]
+  ll x = 2 * (cnt[v[--l]]++) + 1;
+  curr += x * v[l];
+  return l;
+}
+
+int walkLRight(int l){
+  // removing v[l]
+  ll x = 2 * (cnt[v[l]]--) - 1;
+  curr -= x * v[l];
+  return l + 1;
+}
+
+int main(){
+  iostream::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n, q; cin >> n >> q; v = vector<int>(n); for(auto &x : v) cin >> x;
+  vector<Query> qs(q);
+  for(int i = 0; i < q; ++i){
+    cin >> qs[i].l >> qs[i].r;
+    qs[i].idx = i;
+    qs[i].calcOrder();
+  }
+  sort(qs.begin(), qs.end(), [&](Query &a, Query &b){return a.ord < b.ord;});
+  vector<ll> ans(q);
+  int lastL = 0, lastR = 0;
+  curr = v[0]; cnt[v[0]]++;
+  for(auto q : qs){
+    q.l--; q.r--;
+    // first expand range
+    while(lastR < q.r) lastR = walkRRight(lastR);
+    while(lastL > q.l) lastL = walkLLeft(lastL);
+    // then restrict
+    while(lastR > q.r) lastR = walkRLeft(lastR);
+    while(lastL < q.l) lastL = walkLRight(lastL);
+    ans[q.idx] = curr;
+  }
+  for(auto x : ans) cout << x << "\n";
+}
