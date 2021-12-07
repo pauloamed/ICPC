29be5a8cb87bd50d786b4e65fa88e435cd824899
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2021-12-07 18:54:49 -0300

    segt pers de respeito e uns extras

---
+++ Extras/gaussian_xor.cpp
@@ -0,0 +1,47 @@
+#define MAXN 1510
+ 
+bitset<MAXN> base[MAXN];
+bitset<MAXN> aux[MAXN];
+ 
+int add(bitset<MAXN> &x){
+	bitset<MAXN> auxX(0);
+ 
+	for(int i = 0; i < MAXN; ++i){
+		// checando pos i e pos i no vetor x
+ 
+		if(x[i]){ // tenho bit ativo nessa posicao
+			if(base[i][i]){ // tem um vetor nessa posicao
+				x ^= base[i];
+				auxX ^= aux[i];
+				if(!x.any()) return -1;
+			}else{ // adiciono
+				base[i] = x;
+				auxX[i] = 1;
+				aux[i] = auxX;
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+ 
+bitset<MAXN> query(bitset<MAXN> &x){
+	bitset<MAXN> ans(0);
+	for(int i = 0; i < MAXN; ++i){
+		// checando pos i e pos i no vetor x
+		if(x[i] && base[i][i]){
+			x ^= base[i];
+			ans ^= aux[i];
+		}
+	}
+	return (x.any()? bitset<MAXN>(0) : ans);
+
+  int getMax(){
+    bitset<MAXN> ans;
+    for(int i = 0; i < MAXN; ++i){
+      int j = MAXN - 1 - i;
+      if(base[i][j] && !ans[j]) ans ^= base[i];
+    }
+    return ans.to_ulong();
+  }
+}
--- Extras/kth_on_tree.cpp
+++ Extras/kth_on_tree.cpp
@@ -1,3 +1,9 @@
+// https://www.spoj.com/problems/COT/
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
 template<int MAXN>
 struct PersSegtree{
   using ValType = int;
@@ -105,3 +111,129 @@ struct PersSegtree{
     } 
   }
 };
+
+
+const int MAXV=2e5+10;
+const int MAXLOG=20;
+
+vector<int> v[MAXV];
+
+namespace LCA{
+  int st[MAXV][MAXLOG];
+  int lvl[MAXV];
+
+  void init_dfs(int x, int par){
+    st[x][0] = par;
+    if(par != -1) lvl[x] = lvl[par] + 1;
+    for(auto y : v[x]) if(y != par) init_dfs(y, x);
+  }
+
+  void init_st(){
+    for(int x = 1; x < MAXLOG; ++x){
+      for(int i = 0; i < MAXV; ++i){
+        if(st[i][x-1] == -1) st[i][x] = -1;
+        else st[i][x] = st[st[i][x-1]][x-1];
+      }
+    }
+  }
+
+  void init(int root){
+    init_dfs(root, -1); init_st();
+  }
+
+  int lca(int x, int y){
+    if(lvl[x] < lvl[y]) swap(x, y); // x is deeper
+
+    int falta_subir = (lvl[x] - lvl[y]);
+
+    for(int i = MAXLOG-1; i >= 0; --i){
+      if((1<<i) <= falta_subir){
+        falta_subir -= (1<<i);
+        x = st[x][i];
+      }
+    }
+
+    if(x == y) return x;
+
+    for(int i = MAXLOG-1; i >= 0; --i){
+      if(st[x][i] != st[y][i]){
+        x = st[x][i];
+        y = st[y][i];
+      }
+    }
+    return st[x][0];
+  }
+}
+
+
+
+PersSegtree<MAXV> pers_seg;
+
+vector<int> id2time, par, w;
+
+
+void dfs(int x, int p){
+  par[x] = p;
+  int old_count = pers_seg.query(w[x], w[x], id2time[p]);
+  id2time[x] = pers_seg.update(w[x], old_count + 1, false, id2time[p]);
+
+  for(auto y : v[x]){
+    if(y != p) dfs(y, x);
+  }
+}
+
+
+int32_t main(){
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  int n, q; cin >> n >> q;
+
+  pers_seg.init(0, n*2);
+
+  w = vector<int>(n + 1);
+  id2time = vector<int>(n + 1);
+  par = vector<int>(n + 1);
+
+  for(int i = 1; i <= n; ++i) cin >> w[i];
+
+
+  vector<int> vals_sorted;
+  {
+    vector<int> vals(w);
+    sort(vals.begin(), vals.end());
+    map<int,int> m;
+    for(auto x : vals){
+      if(m.count(x) == 0){
+        m[x] = m.size();
+        vals_sorted.push_back(x);
+      }
+    }
+    for(auto &x : w) x = m[x];
+  }
+
+  for(int i = 1; i < n; ++i){
+    int a, b; cin >> a >> b;
+    v[a].push_back(b);
+    v[b].push_back(a);
+  }
+
+  id2time[0] = 0;
+  dfs(1, 0);
+
+  LCA::init(1);
+
+  while(q--){
+    int u, v, k; cin >> u >> v >> k;
+    int l = LCA::lca(u, v);
+
+    vector<int> pos = {u, v};
+    vector<int> neg = {l, par[l]};
+
+    for(auto &x : pos) x = pers_seg.time2node[id2time[x]];
+    for(auto &x : neg) x = pers_seg.time2node[id2time[x]];
+
+    int kth_idx = pers_seg.kth(k, 0, pers_seg.n - 1, pos, neg);
+
+    cout << vals_sorted[kth_idx] << "\n";
+  }
+}
