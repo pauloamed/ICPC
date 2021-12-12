ad0e158864600435e8e96e62f7f07fe67afd1935
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2021-12-12 20:01:50 -0300

    aho automaton

--- Strings/aho_corasick_automaton.cpp
+++ Strings/aho_corasick_automaton.cpp
@@ -1,17 +1,17 @@
+// http://www.usaco.org/index.php?page=viewproblem2&cpid=533
+
 #include<bits/stdc++.h>
 using namespace std;
 
-#define FIRST_LETTER '0' // first letter of the alphabet
-#define N 100010 // number of states: sum of length of all patterns
-#define ALFA 80 // size of alfabet
 
-
-struct AhoCorasick{
+// N: number of states: sum of length of all patterns
+template<int N, int ALPHA_SIZE, char FIRST_LETTER> 
+struct AhoCorasickAutomaton{
 	// MEMORY: THETA(ALFA * N)
 
 	// TIME:
 	//	addString: THETA(|S|)
-	//  init(): THETA(ALFA * N)
+	//  init(): THETA(ALPHA_SIZE * N)
 
 	// stateCount: number of active states
 	// suffixLink[i]: link to the state S representing the greater preffix that is also a suffix on s[:i]
@@ -26,49 +26,57 @@ struct AhoCorasick{
 
 	// state 0: not a real state, used for marking non init values
 	// state 1: start state
+	int next_node = 2;
+	const int START_STATE = 1;
+	
 
-	int suffixLink[N], term[N];
-	int to[N][ALFA];
-	int stateCount = 1;
+	int to[N][ALPHA_SIZE];
+  int acc[N];
 
-	AhoCorasick(){}
+	AhoCorasickAutomaton(){}
 
-	int addString(const char *str, int id){ // basic trie insertion
-		// (id: could be used for adding the id to a term vector)
-		int curr = 1; // current state: initial state 1
-		int i = 0; // will be used to get the string size
-		for(i = 0; str[i]; i++){ // for each char at the string
-			int ch = str[i] - FIRST_LETTER; // retrive char id
+	int add(string &s, int id){ // basic trie insertion
+		// (id: could be used for adding the id to a acc vector)
+		int curr = START_STATE; // current state: initial state 1
+		
+		for(int i = 0; i < s.size(); i++){ // for each char at the string
+			int ch = s[i] - FIRST_LETTER; // retrive char id
 			// if the following state hasnt been computed yet, create a new one
-			if(!to[curr][ch]) to[curr][ch] = ++stateCount;
+			if(!to[curr][ch]) to[curr][ch] = next_node++;
 			curr = to[curr][ch]; // go to the following state
 		}
-		term[curr]++; // mark that another string ends on this state
+
+    // ATTENTION: PROBLEM SPECIFIC
+    // WHAT SHOULD BE KEPT AT THE ENDING NODE?
+		acc[curr] = s.size();
 		return curr;
 	}
 
+	int suffixLink[N];
+
 	void init(){
 		// init suffixLinks to the starting state
-		for(int i = 0; i < stateCount; i++) suffixLink[i] = 1;
+		for(int i = 0; i < next_node; i++) suffixLink[i] = START_STATE;
 
 		// preparing queue for BFS adding the start state
-		queue<int> q; q.push(1);
+		queue<int> q; q.push(START_STATE);
 
 		while(!q.empty()){ // for all reachable nodes
 			int u = q.front(); q.pop(); // retriving node from bfs' queue
-			for(int i = 0; i < ALFA; i++){ // for each edge (transition) (letter of the alfabet)
+			for(int i = 0; i < ALPHA_SIZE; i++){ // for each edge (transition) (letter of the alfabet)
 				if(to[u][i]){ // if there is an active transition (from trie)
 					int v = to[u][i]; q.push(v); // add it to the queue
-					if(u != 1){
+					if(u != START_STATE){
 						// just as in KMP construction in success case
 						suffixLink[v] = to[suffixLink[u]][i];
 						// if the suffixLink[v] state is a valid string
 						// since s[i-suffixLink[v]:i] = string(suffixLink[v])
 						// Im also a terminal state and represent the same as those from my
 						// suffixlink
-						term[v] += term[suffixLink[v]];
+
+						acc[v] = max(acc[v], acc[suffixLink[v]]);
 					}
-				}else if(u != 1){
+				}else if(u != START_STATE){
 					// I'm not on the start state and there is no active conn:
 					// I will values (DP) from already computed states
 					// as in suffix automata
@@ -76,7 +84,7 @@ struct AhoCorasick{
 				}else{
 					// I'm on the start state and there is no active connection:
 					// I will stay on the start state
-					to[u][i] = 1;
+					to[u][i] = START_STATE;
 				}
 			}
 		}
@@ -87,4 +95,34 @@ struct AhoCorasick{
 	}
 };
 
-AhoCorasick aho; // se botar pra alocar na stack pode dar seg fault kkkk
+AhoCorasickAutomaton<100010, 30, 'a'> aho; 
+
+int main(){
+  freopen("censor.in", "r", stdin);
+  freopen("censor.out", "w", stdout);
+  string t; cin >> t;
+  int n; cin >> n;
+  for(int i = 0; i < n; ++i){
+    string s; cin >> s;
+    aho.add(s, i);
+  }
+
+  aho.init();
+
+	string ans;
+	stack<int> states;
+	states.push(aho.START_STATE);
+	for(int i = 0; i < t.size(); ++i){
+		states.push(aho.go(states.top(), t[i]));
+		ans.push_back(t[i]);
+		if(aho.acc[states.top()]){
+			// cout << "?" << endl;
+			int x = aho.acc[states.top()];
+			while(x--){
+				states.pop();
+				ans.pop_back();
+			}
+		}
+	}
+	cout << ans << endl;
+}
