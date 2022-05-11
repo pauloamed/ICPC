6e70e0d954f741d0a4813e00755eb79900b70b23
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2022-05-11 09:50:56 -0300

    Update sieve_gm.cpp

--- number_theory/sieve_gm.cpp
+++ number_theory/sieve_gm.cpp
@@ -5,13 +5,27 @@ using namespace std;
 
 // minimum prime factors of numbers. init all 0
 int min_prime_fact[MAXN];
+vector<int> pr;
+
+int sieve(int n){
+  for(int i = 2; i < n; ++i){
+    if(min_prime_fact[i] == 0){
+      min_prime_fact[i] = i;
+      pr.push_back(i);
+    }
+    for(int j = 0; j < pr.size(); j++){
+      if(i * pr[j] > n) break;
+      min_prime_fact[i * pr[j]] = pr[j];
+      if(pr[j] == min_prime_fact[i]) break;
+    }
+  }
+}
 
 // to be used for multiplicative functions
 int count_exp_min_prime_fact[MAXN];
 int mob[MAXN];
 int phi[MAXN]; // phi multiplacative function / euler totient
 
-vector<int> pr; // list of prime numbers
 
 // one can use min_prime_fact for factorization
 // while(x != 1) x /= min_prime_fact[x]
@@ -19,39 +33,37 @@ vector<int> pr; // list of prime numbers
 // for enhancing performance
 
 void sieve(int n){
-    phi[1] = 1;
-    mob[1] = 1;
-    for(int i = 2; i <= n; ++i){
+  phi[1] = 1;
+  mob[1] = 1;
+  for(int i = 2; i < n; ++i){
+    if(min_prime_fact[i] == 0){ // prime!
+      phi[i] = i - 1; // def of phi
+      mob[i] = -1;
+      min_prime_fact[i] = i; // itself
+      count_exp_min_prime_fact[i] = 1; // itself
 
-        if(min_prime_fact[i] == 0){ // prime!
-            phi[i] = i - 1; // def of phi
-            mob[i] = -1;
-            min_prime_fact[i] = i; // itself
-            count_exp_min_prime_fact[i] = 1; // itself
-
-            pr.push_back(i);
-        }
-        // for all primes less than or eq i, fill their product by i into min_prime_fact
-        for(int j = 0; j < pr.size(); j++){
-            if(i * pr[j] > n) break; // out of range
-            min_prime_fact[i * pr[j]] = pr[j];
-
-            if(pr[j] == min_prime_fact[i]){
-              phi[i * pr[j]] = phi[i] * pr[j];
-              mob[i * pr[j]] = 0;
-              // since i and i*pr[j] have the same min count fact (pr[j])
-              // (but i has already some pr[j] as factors)
-              count_exp_min_prime_fact[i * pr[j]] = count_exp_min_prime_fact[i] + 1;
-              break;
-            }else{
-              // i and pr[j] are coprime. thus, i * pr[j] has only one pr[j] factor
-	            mob[i * pr[j]] = mob[i] * mob[pr[j]];
-              phi[i * pr[j]] = phi[i] * phi[pr[j]];
-
-              count_exp_min_prime_fact[i * pr[j]] = 1;
-            }
-        }
+      pr.push_back(i);
     }
+    // for all primes less than or eq i, fill their product by i into min_prime_fact
+    for(int j = 0; j < pr.size(); j++){
+      if(i * pr[j] > n) break; // out of range
+      min_prime_fact[i * pr[j]] = pr[j];
+
+      if(pr[j] == min_prime_fact[i]){
+        phi[i * pr[j]] = phi[i] * pr[j];
+        mob[i * pr[j]] = 0;
+        // since i and i*pr[j] have the same min count fact (pr[j])
+        // (but i has already some pr[j] as factors)
+        count_exp_min_prime_fact[i * pr[j]] = count_exp_min_prime_fact[i] + 1;
+        break;
+      }else{
+        // i and pr[j] are coprime. thus, i * pr[j] has only one pr[j] factor
+        mob[i * pr[j]] = mob[i] * mob[pr[j]];
+        phi[i * pr[j]] = phi[i] * phi[pr[j]];
+        count_exp_min_prime_fact[i * pr[j]] = 1;
+      }
+    }
+  }
 }
 
 int mult(int x, int n){
