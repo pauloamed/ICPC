861353564389f2e04eec30204e07feca2a847f4f
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2022-05-20 16:57:26 -0300

    Update aho_corasick_automaton.cpp

--- strings/aho_corasick_automaton.cpp
+++ strings/aho_corasick_automaton.cpp
@@ -1,8 +1,8 @@
-// http://www.usaco.org/index.php?page=viewproblem2&cpid=533
-
 #include<bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+const int MAXN = 100010;
 
 // N: number of states: sum of length of all patterns
 template<int N, int ALPHA_SIZE, char FIRST_LETTER> 
@@ -22,72 +22,84 @@ struct AhoCorasickAutomaton{
 	// addString: adds a string to the trie
 	// init: prepare the automaton for being use. Uses a BFS
 
-	// #####: once you add a string, you need to use init() once again
+	// #####: once you add a new string, you need to use init() once again
 
-	// state 0: not a real state, used for marking non init values
-	// state 1: start state
-	int next_node = 2;
+	// STATE 0: not a real state, used for marking non init values
+	int next_node;
 	const int START_STATE = 1;
-	
-
+  int suffixLink[N];
 	int to[N][ALPHA_SIZE];
-  int acc[N];
+
+  struct Node{
+    int pts, occur;
+    Node(int pt=0, int occ=0){ pts = pt, occur = occ; }
+  };
+  Node acc[N];
 
 	AhoCorasickAutomaton(){}
 
-	int add(string &s, int id){ // basic trie insertion
-		// (id: could be used for adding the id to a acc vector)
-		int curr = START_STATE; // current state: initial state 1
+  void build(int n){
+    next_node = 2;
+    for(int i = 0; i < n; ++i) memset(to[i], 0, sizeof to[i]);
+  }
+
+	int add(string &s, vector<int> &pts){ // string and string_id
+		int curr = START_STATE;
 		
-		for(int i = 0; i < s.size(); i++){ // for each char at the string
-			int ch = s[i] - FIRST_LETTER; // retrive char id
-			// if the following state hasnt been computed yet, create a new one
-			if(!to[curr][ch]) to[curr][ch] = next_node++;
-			curr = to[curr][ch]; // go to the following state
+		for(int i = 0; i < s.size(); i++){
+			int ch = s[i] - FIRST_LETTER;
+			if(!to[curr][ch]){
+        to[curr][ch] = next_node++;
+        acc[to[curr][ch]] = Node();
+      }
+      curr = to[curr][ch];
+
+      // adding all prefixes
+      acc[curr].occur++;
+      acc[curr].pts = max(acc[curr].pts, pts[i]);
 		}
 
-    // ATTENTION: PROBLEM SPECIFIC
-    // WHAT SHOULD BE KEPT AT THE ENDING NODE?
-		acc[curr] = s.size();
+		// acc[curr] = s.size(); // may add only the whole string also
 		return curr;
 	}
 
-	int suffixLink[N];
+  vector<int> sl_tree[N];
+  void traverse_tree(int x){
+    // cout << x << " " << acc[x].pts << endl;
+    for(auto y : sl_tree[x]){
+      traverse_tree(y);
+      acc[x].pts = max(acc[x].pts, acc[y].pts);
+    }
+    // cout << x << " " << acc[x].pts << endl;
+  }
 
 	void init(){
-		// init suffixLinks to the starting state
-		for(int i = 0; i < next_node; i++) suffixLink[i] = START_STATE;
-
-		// preparing queue for BFS adding the start state
+		for(int i = 0; i < next_node; i++){
+      suffixLink[i] = START_STATE;
+      sl_tree[i].clear();
+    }
 		queue<int> q; q.push(START_STATE);
 
-		while(!q.empty()){ // for all reachable nodes
-			int u = q.front(); q.pop(); // retriving node from bfs' queue
-			for(int i = 0; i < ALPHA_SIZE; i++){ // for each edge (transition) (letter of the alfabet)
-				if(to[u][i]){ // if there is an active transition (from trie)
-					int v = to[u][i]; q.push(v); // add it to the queue
+		while(!q.empty()){
+			int u = q.front(); q.pop();
+			for(int i = 0; i < ALPHA_SIZE; i++){
+				if(to[u][i]){ // existent transition from `u` using `i`
+					int v = to[u][i]; q.push(v);
+          
 					if(u != START_STATE){
 						// just as in KMP construction in success case
-						suffixLink[v] = to[suffixLink[u]][i];
-						// if the suffixLink[v] state is a valid string
-						// since s[i-suffixLink[v]:i] = string(suffixLink[v])
-						// Im also a terminal state and represent the same as those from my
-						// suffixlink
-
-						acc[v] = max(acc[v], acc[suffixLink[v]]);
+						suffixLink[v] = to[suffixLink[u]][i]; // since s[i-suffixLink[v]:i] = string(suffixLink[v])
+            // do some suffix link stuff
 					}
-				}else if(u != START_STATE){
-					// I'm not on the start state and there is no active conn:
-					// I will values (DP) from already computed states
-					// as in suffix automata
-					to[u][i] = to[suffixLink[u]][i];
-				}else{
-					// I'm on the start state and there is no active connection:
-					// I will stay on the start state
-					to[u][i] = START_STATE;
-				}
+          sl_tree[suffixLink[v]].push_back(v);
+				}else{ // inexistent transition from `u` using `i`, if theres a suffixlink, uses it
+          if(u != START_STATE) to[u][i] = to[suffixLink[u]][i];
+          else to[u][i] = START_STATE;
+        }
 			}
 		}
+
+    traverse_tree(START_STATE);
 	}
 
 	int go(int x, char c){
@@ -95,34 +107,38 @@ struct AhoCorasickAutomaton{
 	}
 };
 
-AhoCorasickAutomaton<100010, 30, 'a'> aho; 
+AhoCorasickAutomaton<MAXN, 30, 'a'> aho; 
 
 int main(){
-  freopen("censor.in", "r", stdin);
-  freopen("censor.out", "w", stdout);
-  string t; cin >> t;
-  int n; cin >> n;
-  for(int i = 0; i < n; ++i){
-    string s; cin >> s;
-    aho.add(s, i);
+  freopen("exciting.in", "r", stdin);
+  int t; cin >> t;
+  while(t--){
+    int n; cin >> n;
+    int tot_sz = 0;
+    vector<string> vs(n); 
+    for(auto &s : vs){
+      cin >> s;
+      tot_sz += s.size();
+    }
+    aho.build(tot_sz + 10);
+
+    for(int i = 0; i < n; ++i){
+      vector<int> v(vs[i].size());
+      for(auto &x : v) cin >> x;
+      aho.add(vs[i], v);
+    }
+    aho.init();
+
+    ll ans = 0;
+    for(int j = 0; j < n; ++j){
+      auto &s = vs[j];
+      int curr = aho.START_STATE;
+      for(int i = 0; i < s.size(); ++i){
+        curr = aho.go(curr, s[i]);
+        ll val = 1LL * aho.acc[curr].occur * aho.acc[curr].pts * (i + 1);
+        ans = max(ans, val);
+      }
+    }
+    cout << ans << endl;
   }
-
-  aho.init();
-
-	string ans;
-	stack<int> states;
-	states.push(aho.START_STATE);
-	for(int i = 0; i < t.size(); ++i){
-		states.push(aho.go(states.top(), t[i]));
-		ans.push_back(t[i]);
-		if(aho.acc[states.top()]){
-			// cout << "?" << endl;
-			int x = aho.acc[states.top()];
-			while(x--){
-				states.pop();
-				ans.pop_back();
-			}
-		}
-	}
-	cout << ans << endl;
 }
