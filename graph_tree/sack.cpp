e042c5eacd9f7a97b30a841bcb5856360ddec3c5
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2022-05-25 11:08:43 -0300

    Update sack.cpp

--- graph_tree/sack.cpp
+++ graph_tree/sack.cpp
@@ -1,64 +1,60 @@
-#include<bits/stdc++.h>
-using namespace std;
+// each node is visited by O(logN) calls as a light node
+// until it turns a heavy node
 
-#define MAXN 100010
-
-// global time var for computing the following arrays
-// (only incremented when entering node)
-int currTime;
 // inTime[v]: time in which node v was entered
 // outTime[v]: time in which node v was left
 // time2node[t]: node which was entered in time t
 // inTime[time2Node[t]] = t // time2Node[inTime[v]] = v
-int inTime[MAXN], outTime[MAXN], time2Node[MAXN];
-int sz[MAXN]; // sz[v]: size of subtree rooted at v
-vector<int> v[MAXN]; // adj list
+int curr_time;
+int in_time[MAXN], out_time[MAXN], time2node[MAXN];
 
-int depth[MAXN]; // depth[v] = depth of node v
-int c[MAXN]; // c[v]: char at node v
+int sz[MAXN];
+vector<int> v[MAXN];
 
-void precalc(int x, int par, int d){
-  time2Node[currTime] = x; inTime[x] = currTime++;
+int depth[MAXN];
+
+void precalc(int x, int par=-1, int d=0){
+  time2node[curr_time] = x; in_time[x] = curr_time++;
   depth[x] = d;
   sz[x] = 1;
-  for(auto y : v[x]){
-    if(y == par) continue;
+  for(auto y : v[x]) if(y != par) {
     precalc(y, x, d + 1);
     sz[x] += sz[y];
   }
-  outTime[x] = currTime;
+  out_time[x] = curr_time;
 }
 
-map<int, int> names[MAXN]; // bitset for each depth
-vector<pair<int,int>> queries[MAXN]; // queries at node x
-vector<int> ans; // ans for each query
+void solve(int x, int par=-1, bool keep=0){
+  int heavy_child = -1;
+  for(auto y : v[x]) if(y != par) {
+    if(heavy_child == -1 || sz[y] > sz[heavy_child]) 
+      heavy_child = y;
+  }
 
-void solve(int x, bool keep){ // maybe worry about parent
-  if(v[x].size() > 0){
-    int heavy_child = -1;
-    for(auto y : v[x]){
-      if(sz[y] > sz[heavy_child]) heavy_child = y;
+  if(heavy_child != -1){
+    // solve but dont keep light nodes
+    for(auto y : v[x]) if(y != par && y != heavy_child)
+      solve(y, x, false);
+    // solve and keep heavy nodes
+    solve(heavy_child, x, true);
+  }
+
+  for(auto y : v[x]) if(y != par && y != heavy_child) {
+    // maybe solve queries (light -> (heavy + past_lights))
+    for(int t = in_time[y]; t < out_time[y]; ++t){
+      int z = time2node[t];
+      // query z
     }
- 
-    // vising light and then heavy childs of x
-    for(auto y : v[x]){
-      if(y != heavy_child) solve(y, false);
-    }
-    solve(heavy_child, true);
- 
-    for(auto y : v[x]){
-      if(y == heavyChild || y == par) continue;
-      for(int t = in_time[y]; t < out_time[y]; ++t){
-        int z = time2node[t];
-        // add z
-      }
+    // add light nodes after solving, avoid info leakage
+    for(int t = in_time[y]; t < out_time[y]; ++t){
+      int z = time2node[t];
+      // add z
     }
   }
  
-  // add x
- 
-  // solve queries
- 
+  // solve queries (x -> all subtrees)
+  // add x for querying or for keeping as heavy child
+
   if(!keep){
     for(int t = in_time[x]; t < out_time[x]; ++t){
       int z = time2node[t];
