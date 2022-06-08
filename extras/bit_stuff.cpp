00b18763c26aa9108f61de82f06fa5e0ebbdbbd9
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2022-06-08 12:26:30 -0300

    Create bit_stuff.cpp

---
+++ extras/bit_stuff.cpp
@@ -0,0 +1,15 @@
+// enumerating submasks
+  for(int msk = 1; msk < masks; ++msk){
+    for(int smsk = msk; smsk; smsk = (smsk - 1) & msk){
+      // smsk is submsk of msk
+    }
+  }
+
+
+// (x - 1) & y : iterates through submasks of y
+// (x + 1) | y : iterates through supermasks of y
+
+// least significant bit of x: x & (-x)
+
+// __builtin_popcount: counts active bits of int
+// __builtin_popcountll: counts active bits of long long
