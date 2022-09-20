84f6770ae12b197e96fba14d56cfcab156ea2c6d
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2022-09-20 14:13:44 -0300

    Create extended_gcd.cpp

---
+++ number_theory/extended_gcd.cpp
@@ -0,0 +1,14 @@
+// https://cp-algorithms.com/algebra/extended-euclid-algorithm.html#algorithm
+
+int gcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
