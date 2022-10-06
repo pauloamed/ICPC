2b0e54bf9c27139aead515e151a8bc7321cf1f87
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2022-10-06 15:44:37 -0300

    Update gaussian_xor.cpp

--- gauss_matrix/gaussian_xor.cpp
+++ gauss_matrix/gaussian_xor.cpp
@@ -1,48 +1,76 @@
-#define MAXN 1510
- 
-bitset<MAXN> base[MAXN];
-bitset<MAXN> aux[MAXN];
- 
-int add(bitset<MAXN> &x){
-	bitset<MAXN> auxX(0);
- 
-	for(int i = 0; i < MAXN; ++i){
-		// checando pos i e pos i no vetor x
- 
-		if(x[i]){ // tenho bit ativo nessa posicao
-			if(base[i][i]){ // tem um vetor nessa posicao
-				x ^= base[i];
-				auxX ^= aux[i];
-				if(!x.any()) return -1;
-			}else{ // adiciono
-				base[i] = x;
-				auxX[i] = 1;
-				aux[i] = auxX;
-				return i;
-			}
-		}
-	}
-	return -1;
-}
- 
-bitset<MAXN> query(bitset<MAXN> &x){
-	bitset<MAXN> ans(0);
-	for(int i = 0; i < MAXN; ++i){
-		// checando pos i e pos i no vetor x
-		if(x[i] && base[i][i]){
-			x ^= base[i];
-			ans ^= aux[i];
-		}
-	}
-	return (x.any()? bitset<MAXN>(0) : ans);
+const int MAXN = 1500;
 
-}
+namespace xor_base{
+  // number of vectors in basis
+  // size of vector space is 2^cnt
+  int cnt = 0;
+  bitset<MAXN> base[MAXN];
+  bitset<MAXN> aux[MAXN];
+  
+  // adds X as basis of vector space
+  // returns -1 if it is already inside the span
+  // runs in O(N^2/64)
+  int add(bitset<MAXN> &x){
+    bitset<MAXN> auxX(0);
+    for(int i = MAXN - 1; i >= 0; --i){
+      if(!x[i]) continue;
+      if(!base[i][i]){ cnt++;
+        base[i] = x, auxX[i] = 1;
+        aux[i] = move(auxX);
+        return i;
+      }
+      x ^= base[i];
+      auxX ^= aux[i];
+    }
+    return -1;
+  }
+  
+  // checks if X is inside the span
+  // if it is, returns the linear combination of base vectors
+  // runs in O(N^2/64)
+  bitset<MAXN> query(bitset<MAXN> &x){
+    bitset<MAXN> ans(0);
+    for(int i = MAXN - 1; i >= 0; --i){
+      if(x[i] && base[i][i]){
+        x ^= base[i];
+        ans ^= aux[i];
+      }
+    }
+    return (x.any()? bitset<MAXN>(0) : ans);
+  }
 
-int getMax(){
-	bitset<MAXN> ans;
-	for(int i = 0; i < MAXN; ++i){
-		int j = MAXN - 1 - i;
-		if(base[i][j] && !ans[j]) ans ^= base[i];
-	}
-	return ans.to_ulong();
+  // get maximum value inside the span
+  // runs in O(N^2/64)
+  int get_max(){
+    bitset<MAXN> ans;
+    for(int i = MAXN - 1; i >= 0; --i){
+      if(base[i][i] && !ans[i]){
+        ans ^= base[i];
+      }
+    }
+    return ans.to_ulong();
+  }
+
+  // get kth value inside the span
+  // runs in O(N^2/64)
+  int get_kth(int k){
+    int curr_cnt = cnt;
+
+    bitset<MAXN> ans;
+    for(int i = MAXN - 1; i >= 0; --i){
+      if(!base[i][i]) continue;
+      int half = (1 << (curr_cnt - 1));
+      if(!ans[i]){
+        if(k > half){
+          ans ^= base[i];
+          k -= half;
+        }
+      }else{
+        if(k <= half) ans ^= base[i];
+        else k -= half;
+      }
+      curr_cnt--;
+    }
+    return ans.to_ulong();
+  }
 }
