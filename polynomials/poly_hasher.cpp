f5a67cfee06d185e01898865d387e388aa8cbb01
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2023-02-06 11:21:37 -0300

    Create poly_hasher.cpp

---
+++ polynomials/poly_hasher.cpp
@@ -0,0 +1,24 @@
+const int NUM_HASHES = 3;
+
+struct PolyHasher{
+  vector<mint> coef[NUM_HASHES];
+
+  PolyHasher(int degree){
+    for(int i = 0; i < NUM_HASHES; ++i){
+      coef[i] = vector<mint>(degree + 1);
+      
+      for(int j = 0; j <= degree; ++j){
+        uniform_int_distribution<int> dist(0, MOD - 1);
+        coef[i][j] = dist(rng);
+      }
+    }
+  }
+
+  array<mint, NUM_HASHES> eval(mint x){
+    array<mint, NUM_HASHES> out;
+    for(int i = 0; i < NUM_HASHES; ++i){
+      for(auto c : coef[i]) out[i] = out[i] * x + c;
+    }
+    return out;
+  }   
+};
