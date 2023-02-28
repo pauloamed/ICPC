b595d60d4fde065a927881703d01ce8147f7b58c
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2023-02-28 16:47:23 -0300

    Update sparse_table.cpp

--- data_structures/sparse_table.cpp
+++ data_structures/sparse_table.cpp
@@ -18,7 +18,7 @@ struct SparseTable{
 
     // below only for idempotent functions
     vlog[1] = 0;
-    for(int i = 2; i <= MAXN; i++) vlog[i] = vlog[i/2] + 1; 
+    for(int i = 2; i <= n; i++) vlog[i] = vlog[i/2] + 1; 
   }
 
   int query(int l, int r){ // idempotent
